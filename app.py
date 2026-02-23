0d0281c507463a1effc2046d1d8d1ee789dc55a8
Patrick BIYAGA <biyagapatrick@gmail.com>
2026-02-23 14:38:37 +0100

    Change contact route to render contact.html

--- app.py
+++ app.py
@@ -10,14 +10,13 @@ def hello_world():
 # Déposez votre code à partir d'ici :
 @app.route("/contact")
 def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>" 
+    return render_template("contact.html")
 
 @app.get("/paris")
 def api_paris():
     url = "https://api.open-meteo.com/v1/forecast?latitude=48.8566&longitude=2.3522&hourly=temperature_2m"
     response = requests.get(url)
     data = response.json()
-
     times = data.get("hourly", {}).get("time", [])
     temps = data.get("hourly", {}).get("temperature_2m", [])
 
@@ -26,7 +25,6 @@ def api_paris():
         {"datetime": times[i], "temperature_c": temps[i]}
         for i in range(n)
     ]
-
     return jsonify(result)
     
 @app.route("/rapport")
